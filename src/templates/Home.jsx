f2e1a69559599518576b8bc206e82fba9158d7ca
jongmok1031 <jongmok1031@gmail.com>
2020-10-19 17:02:53 +0900

    router connnected to backend

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import './table.style.css'
+import axios from 'axios'
 
-const Home = () => <>Home 홈페이지에 있습니다</>
+const Home = () => {
+
+    const test = () => {
+        axios.get(`http://localhost:8000/api`)
+        .then(res => {
+            alert(`Connection Success!!`)
+        }
+        ).catch(
+            e => alert(`Failure`)
+        )
+    }
+
+    return (<>
+        <table className = 'tab_layer'>
+        <tr><td><h1>Home</h1></td></tr>
+        <tr><td><button onClick={test}>Connection</button></td></tr>
+        </table>
+
+    </>)
+}
 
 export default Home
